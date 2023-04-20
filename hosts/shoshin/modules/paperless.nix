60a84136e242cb9dbe9a50e2c32a67d332a19168
shosh1n <hermannschris@googlemail.com>
2023-04-20 18:49:18 +0200

    pTa

--- hosts/shoshin/modules/paperless.nix
+++ hosts/shoshin/modules/paperless.nix
@@ -2,11 +2,11 @@
 
 {
   modules.services.paperless.enable = true;
-
+  age.secrets.paperlessauth.file = ../secrets/paperlessauth.age;
   services.paperless = {
     address = "0.0.0.0";
     mediaDir = "/data/media/docs/paperless";
     consumptionDir = "/data/media/docs/consume";
-    passwordFile = "config.age.secrets.paperlessauth.path;";
+    passwordFile = "config.age.secrets.paperlessauth.path";
   };
 }
