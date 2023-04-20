73e306e161a5dd3d5600296c3d505a65b06c003f
shosh1n <hermannschris@googlemail.com>
2023-04-20 15:33:47 +0200

    paperless:crash; fix:try other filename

--- hosts/shoshin/secrets/secrets.nix
+++ hosts/shoshin/secrets/secrets.nix
@@ -2,5 +2,5 @@
     key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJKFmXmnKqwP7SlR7epj8Y3xQQXTRrZZn+/N8uWBumpI hermannschris@googlemail.com";
   
   in {
-    "paperless-auth.age".publicKeys = [ key ];
+    "paperlessauth.age".publicKeys = [ key ];
     }
