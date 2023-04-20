1aa250f71d7bba59802986c3f18c124a51803071
shosh1n <hermannschris@googlemail.com>
2023-04-20 19:50:14 +0200

    trying w/o passworod .ssh

--- hosts/shoshin/secrets/secrets.nix
+++ hosts/shoshin/secrets/secrets.nix
@@ -1,5 +1,6 @@
   let 
-    key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJKFmXmnKqwP7SlR7epj8Y3xQQXTRrZZn+/N8uWBumpI hermannschris@googlemail.com";
+   key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIClElwl33CnzGzW6IEcPsbwdqAkDFhst/TzUZUGMwwTb shoshin";
+
 in
   {
     "paperlessauth.age".publicKeys = [ key ];
