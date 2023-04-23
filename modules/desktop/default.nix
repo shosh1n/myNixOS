9308dd23307039a8851e8afd955147df6773b4fc
shosh1n <hermannschris@googlemail.com>
2023-04-23 20:08:55 +0200

    hyprland

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -4,30 +4,37 @@ with lib;
 with lib.my;
 let cfg = config.modules.desktop;
 in {
-  config = mkIf config.services.xserver.enable {
-    assertions = [
-      {
+
+
+
+  config = mkIf cfg.enable
+    (mkMerge [
+      (mkIf config.wayland.windowManager.enable {
+        user.packages = with pkgs; [
+          pamixer
+        ];
+      })
+
+      (mkIf config.services.xserver.enable {
+       assertions = [
+        {
         assertion = (countAttrs (n: v: n == "enable" && value) cfg) < 2;
         message = "Can't have more than one desktop environment enabled at a time";
-      }
-      {
-        assertion =
-          let srv = config.services;
-          in srv.xserver.enable ||
-             srv.sway.enable ||
-             !(anyAttrs
-               (n: v: isAttrs v &&
-                      anyAttrs (n: v: isAttrs v && v.enable))
-               cfg);
+        }
+        {
+        assertion = let srv = config.services;
+                    in srv.xserver.enable || srv.sway.enable ||
+             !(anyAttrs (n: v: isAttrs v && anyAttrs (n: v: isAttrs v && v.enable)) cfg);
         message = "Can't enable a desktop app without a desktop environment";
-      }
-    ];
+        }
+       ];
 
     user.packages = with pkgs; [
       feh       # image viewer
       xclip
       xdotool
       xorg.xwininfo
+
       libqalculate  # calculator cli w/ currency conversion
       (makeDesktopItem {
         name = "scratch-calc";
@@ -119,5 +126,6 @@ in {
     #  [ -s .xsession-errors ] || rm -f .xsession-errors*
     #  popd
     #'';
-  };
+      })
+    ]);
 }
