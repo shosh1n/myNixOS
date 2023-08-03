893c3972edc4da3b132acee4b5daa11a6dcbeca0
shosh1n <hermannschris@googlemail.com>
2023-08-03 20:11:51 +0200

    bla

---
+++ lib/paths.nix
@@ -0,0 +1,14 @@
+{
+  self,
+  lib,
+  ...
+}:
+with builtins;
+with lib; rec {
+  # ...
+  dotFilesDir = toString ../.;
+  modulesDir = "${dotFilesDir}/modules";
+  configDir = "${dotFilesDir}/config";
+  binDir = "${dotFilesDir}/bin";
+  themesDir = "${modulesDir}/themes";
+}
